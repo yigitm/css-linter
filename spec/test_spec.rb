3616c1a443578a8c5cb3446b5865283bf7f5cad3
Yigit Mersin <yigitmersin@gmail.com>
2020-08-14 12:43:10 +0300

    Add commit before branch merge

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,26 +1,26 @@
 require_relative '../lib/file_reader.rb'
 require_relative '../lib/linters.rb'
-
-  # describe FileReader  do
-  #   subject(:file_read) { FileReader.new }
-  #   context "if CSS file is read" do
-  #     it "should return 'true'" do
-  #       expect(file_read.file_read.class).to be(String)
-  #     end
-  #   end
-  # end
+  
+  describe FileReader do
+    let(:lint) { Linters.new }
+    context "if CSS file is read successfully" do
+      it "should return a 'String' value" do
+        expect(lint.file_read.class).to be(String)
+      end
+    end
+  end
  
   describe Linters < Prompter do
     let(:lint) { Linters.new }
     context "if both brackets are matched" do
       it "should return 'Passed' message" do
-        expect(lint.count_tag).to eq(@passed)
+        expect(lint.tag_checker).to eq(@passed)
       end
     end
 
     context "if both brackets are matched" do
       it "should return 'Failed' message" do
-        expect(lint.count_tag).to eq(@failed)
+        expect(lint.tag_checker).to eq(@failed)
       end
     end
   end
