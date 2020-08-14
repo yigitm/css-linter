d639c6c285432fd81bbdac8009a5b257edaa44fa
Yigit Mersin <yigitmersin@gmail.com>
2020-08-14 16:37:16 +0300

    Update Linters class tag checker method

--- lib/linters.rb
+++ lib/linters.rb
@@ -8,17 +8,40 @@ attr_reader :file_data,:open_bracket,:closing_bracket,:result
     @closing_bracket = closing_bracket
     @result = result
   end
-  
-  def tag_checker
-    @file_data = file_read
-    @open_bracket = (/{/)
-    @closing_bracket = (/}/)
-    if @file_data.match(@open_bracket).to_s == "{" && @file_data.match(@closing_bracket).to_s == "}" 
+    
+    # if @file_data.match(@open_bracket).to_s == "{" && @file_data.match(@closing_bracket).to_s == "}" 
+    #     prompt_message('passed')
+    #     true
+    # else
+    #     prompt_message('failed')
+    #     false
+    # end
+    
+    #toplam parantez sayısı çift olması 
+    #hiç parantez olmaması
+    #duplike parantez olması durumu
+    #bir içerik te olup diğerinde olmaması
+
+    def missing_tag_checker
+      @file_data = file_read
+      @open_bracket = (/{/)
+      @closing_bracket = (/}/)
+      @duplicate_open = (/{{/)
+      @duplicate_close = (/}}/)
+      
+      matchs = @file_data.scan(@open_bracket).size + @file_data.scan(@closing_bracket).size
+      if matchs.even? && matchs != 0 && @file_data.size != 2 
         prompt_message('passed')
-        true
-    else
+      elsif matchs.odd? == true && matchs != 0
         prompt_message('failed')
-        false
+        prompt_lint('missing_tag')
+      elsif matchs == 0
+        prompt_message('failed')
+        prompt_lint('no_brackets')
+      elsif @file_data.split('').values_at(0) == ['{'] && @file_data.split('').values_at(-1) == ['}'] 
+        prompt_message('passed')
+      end
     end
-  end
+
+
 end
--- lib/prompter.rb
+++ lib/prompter.rb
@@ -1,20 +1,37 @@
 require_relative '../lib/linters.rb'
 class Prompter
-    attr_reader :message,:passed,:failed
+    attr_reader :message,:passed,:failed,
+    :missing_tag,:no_brackets
     def initialize
         @message = message
-        @passed = passed
-        @failed = failed
+        @passed = 'passed'
+        @failed = 'failed'
+        @missing_tag = 'missing_tag'
+        @no_brackets = 'no_brackets'
     end
 
     def prompt_message(message)
         case message 
         when 'passed'
-              @passed = puts "Tag Check Status: Passed"
+            puts "Tag Check Status: Passed"
         when 'failed'
-              @failed = puts "Tag Check Status: Failed"
+            puts "Tag Check Status: Failed"
         else 
             "No message to display!"     
         end
     end
+
+    def prompt_lint(related_lint)
+        case related_lint
+        when 'missing_tag'
+            puts @missing_tag = "Missing bracket(s)"
+        when 'no_brackets'
+            puts @no_brackets = "Missing opening & closing brackets."
+        else 
+            "No message to display!"     
+        end
+    end
+
+
+
 end
