3358312d90997ca43250976e8798faa1e3bc47f0
Yigit Mersin <yigitmersin@gmail.com>
2020-08-16 17:47:51 +0300

    Fix rubocop errors in Rspec tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,120 +1,111 @@
 require_relative '../lib/lint_CSS.rb'
 require_relative '../lib/linters.rb'
-  
-  describe LintCSS do
-    let(:lint) { Linters.new }
-    context "Checks user input & macth it with related string file name" do
-      it "Should return a 'String' value" do
-        expect(lint.file_select.class).to be(String)
-      end
-    end
 
-    context "Takes user input & read the file content" do
-      it "Should return related file content as a 'String' value" do
-        expect(lint.file_read.class).to be(String)
-      end
+describe LintCSS do
+  let(:lint) { Linters.new }
+  context 'Checks user input & macth it with related string file name' do
+    it "Should return a 'String' value" do
+      expect(lint.file_select.class).to be(String)
     end
   end
 
-  describe Linters < Prompter do
-    let(:lint) { Linters.new }
-    describe "#bracket_checker" do
-      context "Checks total brakects number if matchs sum is even & not zero" do
-        it "Should return 'passed' message" do
-          expect(lint.bracket_checker).to eq(@passed)
-        end
-      end
+  context 'Takes user input & read the file content' do
+    it "Should return related file content as a 'String' value" do
+      expect(lint.file_read.class).to be(String)
+    end
+  end
+end
 
-      context "Checks total brakects number if matchs sum is odd & not zero" do
+describe Linters < Prompter do
+  let(:lint) { Linters.new }
+  describe '#bracket_checker' do
+    context 'Checks total brakects number if matchs sum is even & not zero' do
+      it "Should return 'passed' message" do
+        expect(lint.bracket_checker).to eq(@passed)
+      end
+    end
+
+    context 'Checks total brakects number if matchs sum is odd & not zero' do
+      it "Should return 'failed' & 'missing_brackets' message" do
+        expect(lint.bracket_checker).to eq(@failed)
+        expect(lint.bracket_checker).to eq(@missing_brackets)
+      end
+    end
+
+    context 'Checks total brakects number if no matches found' do
+      it "Should return 'failed' & 'no_brackets' message" do
+        expect(lint.bracket_checker).to eq(@failed)
+        expect(lint.bracket_checker).to eq(@no_brackets)
+      end
+    end
+
+    context 'Checks the first & last regexes and matched pairs/(even numbers)' do
+      context 'If first & last regex conditions are true but matchs sum is odd/(not pairs)' do
         it "Should return 'failed' & 'missing_brackets' message" do
           expect(lint.bracket_checker).to eq(@failed)
           expect(lint.bracket_checker).to eq(@missing_brackets)
         end
       end
-
-      context "Checks total brakects number if no matches found" do
-        it "Should return 'failed' & 'no_brackets' message" do
-          expect(lint.bracket_checker).to eq(@failed)
-          expect(lint.bracket_checker).to eq(@no_brackets)
-        end
-      end
-
-      context "Checks the first & last regexes and matched pairs/(even numbers)" do
-        context "If first & last regex conditions are true but matchs sum is odd/(not pairs)" do
-          it "Should return 'failed' & 'missing_brackets' message" do
-            expect(lint.bracket_checker).to eq(@failed)
-            expect(lint.bracket_checker).to eq(@missing_brackets)
-          end
-        end
-      end
     end
-
-    describe "#empty_rule_checker" do
-      context "Checks content between openning & closing brackets and if content is empty" do
-        it "It should returns 'failed' & 'empty_rule' message" do
-          expect(lint.empty_rule_checker).to eq(@failed)
-          expect(lint.empty_rule_checker).to eq(@empty_rule)
-        end
-      end
-
-      context "Checks content between openning & closing brackets and if content is NOT empty" do
-        it "It should returns 'passed' message" do
-          expect(lint.empty_rule_checker).to eq(@passed)
-        end
-      end 
-    end
-
-    describe "#important_tag_checker" do
-      context "Checks the count of !important tag & if count is MORE than 10" do
-        it "It should returns a warning message 'Over usage of '!important' - Recommended Limit: 9'" do
-          expect(lint.important_tag_checker).to eq(@failed)
-          expect(lint.important_tag_checker).to eq(@important_tag)
-        end
-      end
-
-      context "Checks the count of !important tag & if count is LESS than 10" do
-        it "It should returns 'passed' message" do
-          expect(lint.important_tag_checker).to eq(@passed)
-        end
-      end
-    end
-
-    describe "#dry_violation_checker" do
-      context "Checks the all property names If any duplicates are find" do
-        it "It should returns 'failed' & 'DRY violation' message" do
-          expect(lint.dry_violation_checker).to eq(@warning)
-          expect(lint.dry_violation_checker).to eq(@dry_violation)
-        end
-      end
-
-      context "If NO duplicates are find" do
-        it "It should returns 'passed' message" do
-          expect(lint.dry_violation_checker).to eq(@passed)
-        end
-      end
-    end
-
-    describe "#property_name_checker" do
-      context "Checks the all colon(s) & semi-colon(s) in property names and if any missing" do
-        it "It should returns 'failed' & 'Syntax error: Property Name' message" do
-          expect(lint.property_name_checker).to eq(@failed)
-          expect(lint.property_name_checker).to eq(@missing_semicolon)
-        end
-      end
-
-      context "If colon(s) & semi-colon(s) syntax is correct" do
-        it "It should returns 'passed' message" do
-          expect(lint.property_name_checker).to eq(@passed)
-        end
-      end
-    end
-
   end
 
-    
-    
+  describe '#empty_rule_checker' do
+    context 'Checks content between openning & closing brackets and if content is empty' do
+      it "It should returns 'failed' & 'empty_rule' message" do
+        expect(lint.empty_rule_checker).to eq(@failed)
+        expect(lint.empty_rule_checker).to eq(@empty_rule)
+      end
+    end
 
+    context 'Checks content between openning & closing brackets and if content is NOT empty' do
+      it "It should returns 'passed' message" do
+        expect(lint.empty_rule_checker).to eq(@passed)
+      end
+    end
+  end
 
+  describe '#important_tag_checker' do
+    context 'Checks the count of !important tag & if count is MORE than 10' do
+      it "It should returns a warning message 'Over usage of '!important' - Recommended Limit: 9'" do
+        expect(lint.important_tag_checker).to eq(@failed)
+        expect(lint.important_tag_checker).to eq(@important_tag)
+      end
+    end
 
+    context 'Checks the count of !important tag & if count is LESS than 10' do
+      it "It should returns 'passed' message" do
+        expect(lint.important_tag_checker).to eq(@passed)
+      end
+    end
+  end
 
-  
+  describe '#dry_violation_checker' do
+    context 'Checks the all property names If any duplicates are find' do
+      it "It should returns 'failed' & 'DRY violation' message" do
+        expect(lint.dry_violation_checker).to eq(@warning)
+        expect(lint.dry_violation_checker).to eq(@dry_violation)
+      end
+    end
+
+    context 'If NO duplicates are find' do
+      it "It should returns 'passed' message" do
+        expect(lint.dry_violation_checker).to eq(@passed)
+      end
+    end
+  end
+
+  describe '#property_name_checker' do
+    context 'Checks the all colon(s) & semi-colon(s) in property names and if any missing' do
+      it "It should returns 'failed' & 'Syntax error: Property Name' message" do
+        expect(lint.property_name_checker).to eq(@failed)
+        expect(lint.property_name_checker).to eq(@missing_semicolon)
+      end
+    end
+
+    context 'If colon(s) & semi-colon(s) syntax is correct' do
+      it "It should returns 'passed' message" do
+        expect(lint.property_name_checker).to eq(@passed)
+      end
+    end
+  end
+end
