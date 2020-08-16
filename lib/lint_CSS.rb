9fde2f187f46a13a2b79a1cef26af7ebe86684e2
Yigit Mersin <yigitmersin@gmail.com>
2020-08-16 20:10:52 +0300

    Fix rubocop errors & refactor bracket-checker method

--- lib/lint_CSS.rb
+++ lib/lint_CSS.rb
@@ -1,13 +1,10 @@
 module LintCSS
-
   def file_select
-    @file_list = ['lint-bracket-no.css','lint-bracket-missing.css','lint-empty-rule.css',
-        'lint-important.css','lint-dry.css','lint-property.css']
+    @file_list = ['lint-bracket-no.css', 'lint-bracket-missing.css', 'lint-empty-rule.css',
+                  'lint-important.css', 'lint-dry.css', 'lint-property.css']
     input = @selected_file
     @file_list.each do |file|
-      if @file_list.index(file).to_i ==  input.to_i - 1 
-        @file_list[input.to_i - 1]
-      end
+      @file_list[input.to_i - 1] if @file_list.index(file).to_i == input.to_i - 1
     end
     @file_list[input.to_i - 1]
   end
@@ -22,9 +19,8 @@ module LintCSS
     @file_data
   end
 
-
   def bracket_splitter
-    @first_bracket = @file_data.split('').values_at(0) 
+    @first_bracket = @file_data.split('').values_at(0)
     @last_bracket = @file_data.split('').values_at(-1)
   end
 
@@ -33,22 +29,61 @@ module LintCSS
   end
 
   def string_to_array
-    split_each_item = @file_data.split(" ")
+    split_each_item = @file_data.split(' ')
     split_each_item
   end
 
-  def find_bracket_index(bracket_type)
+  def compare_bracket_length
+    if find_same_brackets('{').length == find_same_brackets('}')
+      true
+    elsif find_same_brackets('{').length != find_same_brackets('}')
+      false
+    end
+  end
+
+  def begin_end_bracket_checker
+    string_to_array.each_with_index do |character, _index|
+      true if character.match('{') && character.match('}')
+    end
+  end
+
+  def bracket_even_checker
+    if compare_bracket_length
+      prompt_message('passed')
+    elsif bracket_no_checker
+      prompt_message('failed')
+      prompt_lint('no_brackets')
+    else
+      prompt_message('failed')
+      prompt_lint('missing_brackets')
+    end
+  end
+
+  def bracket_odd_checker
+    if matchs.odd?
+      true if begin_end_bracket_checker
+      prompt_message('failed')
+      prompt_lint('missing_brackets')
+    elsif bracket_no_checker
+      prompt_message('failed')
+      prompt_lint('no_brackets')
+    end
+  end
+
+  def bracket_no_checker
+    true if matchs.zero? || matchs.nil?
+  end
+
+  def find_same_brackets(bracket_type)
     bracket_array = []
-    string_to_array.each_with_index do |character , index|
-      if character.match(bracket_type)
-          bracket_array << index
-      end
+    string_to_array.each_with_index do |character, index|
+      bracket_array << index if character.match(bracket_type)
     end
     bracket_array
   end
 
   def check_fill_or_not(indish_open, indish_close)
-    l = (indish_open.length + indish_close.length) /  2
+    l = (indish_open.length + indish_close.length) / 2
     i = 0
     l.times do
       if indish_close[i] - indish_open[i] <= 1
@@ -70,5 +105,4 @@ module LintCSS
     result = array_one.length - array_two.length
     result
   end
-
 end
--- lib/prompter.rb
+++ lib/prompter.rb
@@ -1,103 +1,95 @@
 require_relative '../lib/linters.rb'
 require 'colorize'
 class Prompter
-    attr_reader :passed,:failed,:warning,
-    :missing_brackets,:no_brackets,:empty_rule,:important_tag,:dry_violation,:file_list
-    # :file_list,:file_bn,:file_bm,:file_e,:file_i,:file_d,:file_p,:selected_file,:valid
-    
-    def initialize
-        @passed = 'passed'
-        @failed = 'failed'
-        @warning = 'warning'
-        @missing_brackets = 'missing_brackets'
-        @no_brackets = 'no_brackets'
-        @empty_rule = 'empty_rule'
-        @important_tag = 'important_tag'
-        @dry_violation = 'dry_violation'
-        @file_list = file_list
-        # @file_bn = 'lint-bracket_no.css'
-        # @file_bm = 'lint-bracket_missing.css'
-        # @file_e = 'lint-empty-rule.css'
-        # @file_i = 'lint-important.css'
-        # @file_d = 'lint-dry.css'
-        # @file_p = 'lint-property.css'
-        @selected_file = selected_file
-        @valid = false
-    end
+  attr_reader :passed, :failed, :warning,
+              :missing_brackets, :no_brackets, :empty_rule, :important_tag, :dry_violation, :file_list
 
-    def prompt_ascii
-      print "+-+-+-+-+".red 
-      print "+-+".green 
-      print "+-+-+-+-+-+-+\n".yellow
-      print"|R|U|B|Y|".red
-      print "|-|".green 
-      print "|L|I|N|T|E|R|\n".yellow
-      print "+-+-+-+-+".red 
-      print "+-+".green
-      print "+-+-+-+-+-+-+\n".yellow
-    end
+  def initialize
+    @passed = 'passed'
+    @failed = 'failed'
+    @warning = 'warning'
+    @missing_brackets = 'missing_brackets'
+    @no_brackets = 'no_brackets'
+    @empty_rule = 'empty_rule'
+    @important_tag = 'important_tag'
+    @dry_violation = 'dry_violation'
+    @file_list = file_list
+    @selected_file = selected_file
+    @valid = false
+  end
 
-    def prompt_welcome
-      loop do
-        prompt_ascii
-        puts "\n\nPlease select any 'file_id' between '1 to 9' for linter check\n".white
-        prompt_file_list
-        @selected_file = gets.chomp
-        if @selected_file.match(/[1-6]/) == nil
-        puts "Invalid entry. Please type/select a number between 1..9"
-        elsif @selected_file.match(/[1-6]/) != nil
-          @valid = true
-          @selected_file
-        end
+  def prompt_ascii
+    print '+-+-+-+-+'.red
+    print '+-+'.green
+    print "+-+-+-+-+-+-+\n".yellow
+    print '|R|U|B|Y|'.red
+    print '|-|'.green
+    print "|L|I|N|T|E|R|\n".yellow
+    print '+-+-+-+-+'.red
+    print '+-+'.green
+    print "+-+-+-+-+-+-+\n".yellow
+  end
+
+  def prompt_welcome
+    loop do
+      prompt_ascii
+      puts "\n\nPlease select any 'file_id' between '1 to 9' for linter check\n".white
+      prompt_file_list
+      @selected_file = gets.chomp
+      if @selected_file.match(/[1-6]/).nil?
+        puts 'Invalid entry. Please type/select a number between 1..9'
+      elsif !@selected_file.match(/[1-6]/).nil?
+        @valid = true
         @selected_file
-        break if @valid == true
       end
+      break if @valid == true
     end
+  end
 
-    def prompt_file_list
-      @file_list = ['lint-bracket-no.css','lint-bracket-missing.css','lint-empty-rule.css',
-        'lint-important.css','lint-dry.css','lint-property.css']
-      @file_list.each_with_index do |file, id|
-        puts "#{id+1}-#{file}\n".light_blue
-      end
+  def prompt_file_list
+    @file_list = ['lint-bracket-no.css', 'lint-bracket-missing.css', 'lint-empty-rule.css',
+                  'lint-important.css', 'lint-dry.css', 'lint-property.css']
+    @file_list.each_with_index do |file, id|
+      puts "#{id + 1}-#{file}\n".light_blue
     end
-    
-    def prompt_message(message)
-        case message 
-        when 'passed'
-            @passed = puts "Check Status: Passed".green
-        when 'failed'
-            @failed = puts "Check Status: Failed".red
-        when 'warning'
-            @warning = puts "Check Status: Warning".yellow
-        else 
-            "No message to display!"     
-        end
-    end
+  end
 
-    def prompt_lint(lint_message)
-        case lint_message
-        when 'missing_brackets'
-          @missing_tag = puts "Missing curly bracket(s)".red
-        when 'no_brackets'
-          @no_brackets = puts "No opening & closing curly brackets.".red
-        when 'empty_rule'
-          @empty_rule = puts "Empty Rule".red
-        when '!important'
-          @important_tag = puts "Over usage of '!important' - Recommended Limit: 9".yellow
-        when 'dry_violation'
-          @dry_violation = puts "DRY violation".yellow
-        when 'property_name'
-          @property_name = puts "Syntax error: Property Name".red
-        else
-          "No message to display!"   
-        end
+  def prompt_message(message)
+    case message
+    when 'passed'
+      @passed = puts 'Check Status: Passed'.green
+    when 'failed'
+      @failed = puts 'Check Status: Failed'.red
+    when 'warning'
+      @warning = puts 'Check Status: Warning'.yellow
+    else
+      'No message to display!'
     end
-    
-    def prompt_stop_execute
-      puts "\nThank you for your time & support\n".white
-      print "|R|U|B|Y| ".red
-      print "|L|I|N|T|E|R| ".yellow
-      abort 'ends...'
+  end
+
+  def prompt_lint(lint_message)
+    case lint_message
+    when 'missing_brackets'
+      @missing_tag = puts 'Missing curly bracket(s)'.red
+    when 'no_brackets'
+      @no_brackets = puts 'No opening & closing curly brackets.'.red
+    when 'empty_rule'
+      @empty_rule = puts 'Empty Rule'.red
+    when '!important'
+      @important_tag = puts "Over usage of '!important' - Recommended Limit: 9".yellow
+    when 'dry_violation'
+      @dry_violation = puts 'DRY violation'.yellow
+    when 'property_name'
+      @property_name = puts 'Syntax error: Property Name'.red
+    else
+      'No message to display!'
     end
+  end
+
+  def prompt_stop_execute
+    puts "\nThank you for your time & support\n".white
+    print '|R|U|B|Y| '.red
+    print '|L|I|N|T|E|R| '.yellow
+    abort 'ends...'
+  end
 end
