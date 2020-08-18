387d39e518c57604a34816b11affb658a97d22c1
Yigit Mersin <yigitmersin@gmail.com>
2020-08-18 22:55:25 +0300

    Test rspec files are separated as: linters_spec.rb & lintcss.rb

--- spec/linters_spec.rb
+++ spec/linters_spec.rb
@@ -1,21 +1,6 @@
 require_relative '../lib/lintcss.rb'
 require_relative '../lib/linters.rb'
 
-describe Lintcss do
-  let(:lint) { Linters.new }
-  context 'Checks user input & macth it with related string file name' do
-    it "Should return a 'String' value" do
-      expect(lint.file_select.class).to be(String)
-    end
-  end
-
-  context 'Takes user input & read the file content' do
-    it "Should return related file content as a 'String' value" do
-      expect(lint.file_read.class).to be(String)
-    end
-  end
-end
-
 describe Linters < Prompter do
   let(:lint) { Linters.new }
   describe '#bracket_checker' do
