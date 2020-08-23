77d901b69e9aad680f763f885b891b5b7c6ba8d6
Yigit Mersin <yigitmersin@gmail.com>
2020-08-23 19:28:37 +0300

    Remove unnecessary instance variables from Linters

--- lib/linters.rb
+++ lib/linters.rb
@@ -1,12 +1,6 @@
 require_relative '../lib/prompter.rb'
 require 'colorize'
 class Linters < Prompter
-  attr_accessor :conditions, :dry_count, :stop_execution
-  def initialize
-    @conditions = conditions
-    @dry_count = dry_count
-    @stop_execution = false
-  end
   
   def which_default_file?(selected_file, file_list)
      file_list.each do |file| 
@@ -27,43 +21,43 @@ class Linters < Prompter
   
   def bracket_checker(selected_file)
     f = file_read(selected_file)
-    @conditions = []
+    conditions = []
     counter = 1
     index = 0
     f.each_line do |line|
       index += 1
       if line.include?('{')
-        @conditions << '{'
+        conditions << '{'
         counter = 0
       elsif line.include?('}')
-        @conditions << '}'
+        conditions << '}'
         counter = 4
       elsif counter == 1
-        @conditions << 'c1'
+        conditions << 'c1'
         counter = 0
         prompt_message('failed')
         prompt_lint_error('missing_brackets')
         puts "check line: #{index}".red
       elsif counter == 2 && line.match?(/\S/)
-        @conditions << 0
+        conditions << 0
       elsif counter == 2
-        @conditions << 'c2'
+        conditions << 'c2'
         counter = 4
         prompt_message('failed')
         prompt_lint_error('missing_brackets')
         puts "check line: #{index}".red
       elsif counter == 0
-        @conditions << 0
+        conditions << 0
         counter = 2
       elsif counter == 4
-        @conditions << 'no-match'
+        conditions << 'no-match'
         counter = 1 
       end
     end
-    if @conditions.none?('c1') && @conditions.none?('c2')
+    if conditions.none?('c1') && conditions.none?('c2')
       print 'Bracket Check / '.green
       prompt_message('passed')
-    elsif @conditions.none?('{') && @conditions.none?('}')
+    elsif conditions.none?('{') && conditions.none?('}')
       puts 
       prompt_message('failed')
       prompt_lint_error('no_brackets')
@@ -72,30 +66,30 @@ class Linters < Prompter
 
   def empty_rule_checker(selected_file)
     f = file_read(selected_file)
-    @conditions = []
+    conditions = []
     counter = 1
     index = 0
     empty_rules = []
     f.each_line do |line|
       index += 1
       if line.include?('.') && counter == 1
-        @conditions << 'passed-one'
+        conditions << 'passed-one'
         counter = 2
       elsif line.include?('.') && counter != 1
-        @conditions << 'passed-one'
+        conditions << 'passed-one'
       elsif line.include?(';') && counter == 2
-        @conditions << 'passed-two'
+        conditions << 'passed-two'
         counter = 1
       elsif line.include?(';') && counter != 2
-        @conditions << 'passed-two'
+        conditions << 'passed-two'
       elsif line.include?('}') && counter == 1
-        @conditions << 'passed-three'
+        conditions << 'passed-three'
       elsif line.include?('}') && counter != 1
         empty_rules << index
-        @conditions << 'passed-three'
+        conditions << 'passed-three'
         counter = 1
       else
-        @conditions << 'no-match'
+        conditions << 'no-match'
       end
     end
       empty_rules.each {|item| item }
@@ -103,52 +97,52 @@ class Linters < Prompter
  
   def property_name_checker(selected_file)
     f = file_read(selected_file)
-    @conditions = []
+    conditions = []
     index = 0
     f.each_line do |line|
       index += 1
        if line.match?(/^+\s*\w/)
          if !(line.match?(/:/)) || !(line.match?(/;/))
-           @conditions << index
+           conditions << index
          end
        end
     end
-    @conditions
+    conditions
   end
 
   def important_tag_checker(selected_file)
     f = file_read(selected_file)
-    @conditions = []
+    conditions = []
     counter = 0
     index = 0
     f.each_line do |line|
       index += 1
       if line.match?('!important')
-        @conditions << index 
+        conditions << index 
       end
     end
 
-    @conditions.length >= 10? false : true
+    conditions.length >= 10? false : true
   end
 
   def dry_violation_checker(selected_file)
     f = file_read(selected_file)
-    @dry_count = []
-    @conditions = []
+    dry_count = []
+    conditions = []
     index = 0
     counter = []
     f.each_line do |line|
       if line.match?(/\S*\w*\s\W*\w*;/)
-        @conditions << line
+        conditions << line
       end
     end
 
     f.each_line do |line|
       index += 1
-      if @conditions.include?(line) && @conditions.count(line) >=2
-        @dry_count << index
+      if conditions.include?(line) && conditions.count(line) >=2
+        dry_count << index
       end
     end
-    @dry_count
+    dry_count
   end
 end
