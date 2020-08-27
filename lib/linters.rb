a7501adefcbd8afadc0b72c3da69139e4dd33eb3
Yigit Mersin <yigitmersin@gmail.com>
2020-08-27 21:03:49 +0300

    Update empty-rule-checker merhod in linters class

--- lib/linters.rb
+++ lib/linters.rb
@@ -25,6 +25,7 @@ class Linters < LintPrivate
     bracket_condition_check(selected_file, open_one, close_one, counter, line_no)
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity
   def empty_rule_checker(selected_file)
     counter = 1
     index = 0
@@ -40,6 +41,7 @@ class Linters < LintPrivate
     end
     empty_rules
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def property_name_checker(selected_file)
     conditions = []
