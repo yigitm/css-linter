8cbe9c02f214e85ef46a3f8b54b79637ac8fd9c6
Yigit Mersin <yigitmersin@gmail.com>
2020-08-28 23:52:29 +0300

    Update linters.rb

--- lib/linters.rb
+++ lib/linters.rb
@@ -25,8 +25,6 @@ class Linters < LintPrivate
     bracket_condition_check(selected_file, open_one, close_one, counter, line_no)
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
   def empty_rule_checker(selected_file)
     counter = 1
     index = 0
@@ -42,8 +40,6 @@ class Linters < LintPrivate
     end
     empty_rules
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/PerceivedComplexity
 
   def property_name_checker(selected_file)
     conditions = []
